546da088538278b4cbcf94d641027fd6aba4735f
jonascavalcante <jonascavalcantejc@gmail.com>
2021-05-07 17:09:59 -0300

    Dados em tela da API e evento de copiar link em click

--- scripts.js
+++ scripts.js
@@ -21,34 +21,147 @@ function menuToggle() {
 
 const apiLink = 'https://api.shrtco.de/v2/';
 
-let shortenLinkAreas = document.querySelector('.shorten-link-areas');
+const shortenLinkAreas = document.querySelector('.shorten-link-areas');
+const inputLink = document.querySelector('.shorten input');
 
-async function apiFunction() {
+const shortenedLinks = JSON.parse(sessionStorage.getItem('shortenedLinks')) || [];
+
+if (shortenedLinks != '') {
     
-    const shortenInputValue = document.querySelector('.shorten input').value;
-    document.querySelector('.link').innerText = shortenInputValue;
-    
-    shortenLinkAreas.classList.add('active');
+    shortenedLinks.forEach(element => {
 
-    let req = await fetch(`${apiLink}shorten?url=${shortenInputValue}`);
-    let res = await req.json();
-    //console.log(res);
+        shortenLinkAreas.classList.add('active');
 
-    let shortenLinkResult = res.result.short_link;
+        let shortenLinkArea = document.createElement('div');
+        shortenLinkArea.classList.add('shorten-link-area');
 
-    document.querySelector('.shorten-link').value = shortenLinkResult;
+        shortenLinkAreas.appendChild(shortenLinkArea);
+        
+        let link = document.createElement('p');
+        link.classList.add('link');
+        link.textContent = element[0];
+        
+        let shortenLink = document.createElement('input');
+        shortenLink.type = 'text';
+        shortenLink.classList.add('shorten-link');
+        shortenLink.value = element[1];
+        shortenLink.readOnly = true;
+
+        let buttonSubmit = document.createElement('button');
+        buttonSubmit.classList.add('button-submit');
+        buttonSubmit.textContent = 'Copy';
+
+        shortenLinkArea.appendChild(link);
+        shortenLinkArea.appendChild(shortenLink);
+        shortenLinkArea.appendChild(buttonSubmit);
+
+        buttonSubmit.addEventListener('click', (e) => {
+            shortenLink.select();
+            document.execCommand('copy');
+            
+            e.target.style.backgroundColor = 'hsl(257, 27%, 26%)';
+            e.target.textContent = 'Copied!';
+            
+            setTimeout(() => {
+                buttonSubmit.style.backgroundColor = 'hsl(180, 66%, 49%)';
+                buttonSubmit.textContent = 'Copy';
+            }, 5000);
+        })
+    });
 }
 
-function copyT(e) {
+function apiFunction(event) {
 
-    let copyText = document.getElementById('shorten-link');
+    event.preventDefault();
+
+    const inputLinkValue = inputLink.value;
+    inputLink.value = '';
+
+    let spanError = document.querySelector('.shorten span');
     
-    copyText.select();
-    document.execCommand('copy');
+    if (inputLinkValue != '') {
+        disabledInputAndButton(event);
+        shortenLinkAreas.classList.add('active');
 
-    e.style.backgroundColor = 'hsl(257, 27%, 26%)';
-    e.textContent = 'Copied!';
-};
-
+        apiFetch(event, inputLinkValue);
+        
+        spanError.classList.remove('error');
+    } else {
+        spanError.classList.add('error');
+    }
+}
 
 /* SHORTEN */
+
+// DESABILITANDO INPUT E BUTTON
+function disabledInputAndButton(event) {
+    inputLink.setAttribute('disabled', 'disable');
+    event.target.disabled = true;
+}
+
+// HABILITANDO INPUT E BUTTON
+function activingInputAndButton(event) {
+    inputLink.removeAttribute('disabled');
+    event.target.disabled = false;
+}
+
+// CRIANDO OS ELEMENTOS
+function createElements(inputLinkValue) {
+    let shortenLinkArea = document.createElement('div');
+    shortenLinkArea.classList.add('shorten-link-area');
+
+    shortenLinkAreas.appendChild(shortenLinkArea);
+    
+    let link = document.createElement('p');
+    link.classList.add('link');
+    link.textContent = inputLinkValue;
+    
+    let shortenLink = document.createElement('input');
+    shortenLink.type = 'text';
+    shortenLink.classList.add('shorten-link');
+    shortenLink.value = 'Loading...';
+    shortenLink.readOnly = true;
+
+    let buttonSubmit = document.createElement('button');
+    buttonSubmit.classList.add('button-submit');
+    buttonSubmit.textContent = 'Copy';
+
+    shortenLinkArea.appendChild(link);
+    shortenLinkArea.appendChild(shortenLink);
+    shortenLinkArea.appendChild(buttonSubmit);
+
+    return { shortenLinkArea, link, shortenLink, buttonSubmit };
+}
+
+function apiFetch(event, inputLinkValue) {
+
+    const { shortenLink, buttonSubmit } = createElements(inputLinkValue);
+
+    fetch(`${apiLink}shorten?url=${inputLinkValue}`)
+        .then(response => response.json())
+        .then(response => {
+
+            let shortenedLink = response.result.short_link;
+            shortenLink.value = shortenedLink;
+
+            // SALVANDO NO LOCAL_STORAGE
+            shortenedLinks.push([inputLinkValue, shortenedLink]);
+            sessionStorage.setItem('shortenedLinks', JSON.stringify(shortenedLinks));
+            
+            activingInputAndButton(event);
+        
+
+            buttonSubmit.addEventListener('click', (e) => {
+                shortenLink.select();
+                document.execCommand('copy');
+                
+                e.target.style.backgroundColor = 'hsl(257, 27%, 26%)';
+                e.target.textContent = 'Copied!';
+                
+                setTimeout(() => {
+                    buttonSubmit.style.backgroundColor = 'hsl(180, 66%, 49%)';
+                    buttonSubmit.textContent = 'Copy';
+                }, 5000);
+            })
+        })
+}
